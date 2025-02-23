584e44fd861f2ddbbe8ae77df6646838d9cb2230
aleksandr <kharitonov_am@bk.ru>
2025-02-23 22:13:04 +0300

    Поправил абстракный класс

--- src/utils_bd.py
+++ src/utils_bd.py
@@ -4,21 +4,21 @@ class abstrate_dbmaneger(ABC):
     '''Абстракный класс по работе с БД'''
 
     @abstractmethod
-    def get_companies_and_vacancies_count():
+    def get_companies_and_vacancies_count(self):
         pass
 
     @abstractmethod
-    def get_all_vacancies():
+    def get_all_vacancies(self):
         pass
 
     @abstractmethod
-    def get_avg_salary():
+    def get_avg_salary(self):
         pass
 
     @abstractmethod
-    def get_vacancies_with_higher_salary():
+    def get_vacancies_with_higher_salary(self):
         pass
 
     @abstractmethod
-    def get_vacancies_with_keyword():
+    def get_vacancies_with_keyword(self):
         pass
