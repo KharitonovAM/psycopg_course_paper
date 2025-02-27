c163b45f514767b087306a384e78475fa68a374a
aleksandr <kharitonov_am@bk.ru>
2025-02-27 21:29:10 +0300

    Реализовал функционал по добавлению базы данныых и созданию в ней таблиц

--- setting/setting.py
+++ setting/setting.py
@@ -3,3 +3,4 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent
 LOG_FILE = Path(BASE_DIR, 'logging', 'log_file.txt')
 DATA_FILENAME = Path(BASE_DIR, 'data', 'datafile.json')
+BD_CONFIG_INI = Path(BASE_DIR, 'database.ini')
--- src/utils_bd.py
+++ src/utils_bd.py
@@ -1,4 +1,7 @@
+import psycopg2
 from abc import ABC, abstractmethod
+
+from setting.db_config import config
 
 class abstrate_dbmaneger(ABC):
     '''Абстракный класс по работе с БД'''
@@ -27,3 +30,75 @@ class abstrate_dbmaneger(ABC):
     def get_vacancies_with_keyword(self):
         '''получает список всех вакансий, в названии которых содержатся переданные в метод слова'''
         pass
+
+class DBManager(abstrate_dbmaneger):
+    '''Класс по взаимодействию с базой данных'''
+
+    def __init__(self, database_name):
+        self.database_name = database_name
+
+    def create_database(self, params):
+        """Создание базы данных и таблиц для сохранения данных о вакансиях и компаниях,
+        если такая БД уже имеется - происходит её удаление и создане новой БД"""
+
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute(f"DROP  DATABASE IF EXISTS {self.database_name}")
+        cur.execute(f"CREATE DATABASE {self.database_name}")
+
+        conn.close()
+
+
+        conn = psycopg2.connect(dbname=self.database_name, **params)
+
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE vacansies (
+                    vacansies_id SERIAL PRIMARY KEY,
+                    vacancy_name VARCHAR(255) NOT NULL,
+                    vacancy_address VARCHAR(255),
+                    salary_from INT,
+                    salary_to INT,
+                    company_name VARCHAR(100)
+                )
+            """)
+
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE companies (
+                    company_id SERIAL PRIMARY KEY,
+                    company_name VARCHAR(255) NOT NULL
+                )
+            """)
+
+        conn.commit()
+        conn.close()
+
+
+    def get_companies_and_vacancies_count(self):
+        '''получает список всех компаний и количество вакансий у каждой компании'''
+        pass
+
+    def get_all_vacancies(self):
+        '''получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию'''
+        pass
+
+
+    def get_avg_salary(self):
+        '''получает среднюю зарплату по вакансиям'''
+        pass
+
+
+    def get_vacancies_with_higher_salary(self):
+        '''получает список всех вакансий, у которых зарплата выше средней по всем вакансиям'''
+        pass
+
+
+    def get_vacancies_with_keyword(self):
+        '''получает список всех вакансий, в названии которых содержатся переданные в метод слова'''
+        pass
+
+l = DBManager('my_test1')
+l.create_database(config())
